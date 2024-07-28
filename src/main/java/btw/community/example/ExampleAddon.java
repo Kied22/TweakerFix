01ba2243340c9dcc9734675449cfd1e4082bf667
Kied22 <jordan_summerhz22@hotmail.com>
2024-07-29 09:57:49 +1000

    Changed internal naming structure

--- src/main/java/btw/community/example/ExampleAddon.java
+++ src/main/java/btw/community/example/ExampleAddon.java
@@ -7,7 +7,7 @@ public class ExampleAddon extends BTWAddon {
     private static ExampleAddon instance;
 
     private ExampleAddon() {
-        super("BTW Fishing and Seed Mod", "0.1.0", "FASM");
+        super("BTW FAST", "1.0.1", "FAST");
     }
 
     @Override
